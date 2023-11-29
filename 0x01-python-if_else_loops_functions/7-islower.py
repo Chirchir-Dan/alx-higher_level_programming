234989637c6fb79b5ad14f3865423593ec406fac
Chirchir-Dan <1danchirchir@gmail.com>
2023-11-29 12:26:25 -0800

    .

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
 
 def islower(c):
-    return ord('a') <= ord(c) <= ord('z')
-
-
-# Test cases
-print(islower('a'))  # True
-print(islower('A'))  # False
-print(islower('z'))  # True
-print(islower('Z'))  # False
-print(islower('1'))  # False
+    return 'a' <= c <= 'z'
