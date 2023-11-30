3f47cbca7ca7a966c6f33b3d4622c4640371362b
Chirchir-Dan <1danchirchir@gmail.com>
2023-11-30 13:01:21 -0800

    edited 5

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,16 +1,5 @@
 #!/usr/bin/python3
-import importlib
-
 if __name__ == "__main__":
-    module_name = 'variable_load_5'
-    variable_name = 'a'
+    from variable_load_5 import a
 
-    try:
-        module = importlib.import_module(module_name)
-        if hasattr(module, variable_name):
-            a_value = getattr(module, variable_name)
-            print(a_value)
-        else:
-            print("Variable 'a' not found in the module.")
-    except ImportError:
-        print(f"Error importing module '{module_name}'.")
+    print(a)
