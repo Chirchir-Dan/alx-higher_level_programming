2eef3e4343de01df37ae3955d43409a8a4de03a3
Chirchir-Dan <1danchirchir@gmail.com>
2023-11-30 11:43:15 -0800

    Wrote file 1 that imports functions from the file calculator_1.py, does some Maths, and prints the result.

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+
+    a = 10
+    b = 5
+
+    addition_result = add(a, b)
+    subtraction_result = sub(a, b)
+    multiplication_result = mul(a, b)
+    division_result = div(a, b)
+
+    print("{:d} + {:d} = {:d}".format(a, b, addition_result))
+    print("{:d} - {:d} = {:d}".format(a, b, subtraction_result))
+    print("{:d} * {:d} = {:d}".format(a, b, multiplication_result))
+    print("{:d} / {:d} = {:d}".format(a, b, division_result))
