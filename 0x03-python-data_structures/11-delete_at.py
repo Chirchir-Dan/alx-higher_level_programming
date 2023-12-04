1fa3edccd4fc4c12074b36181affdeb1749de967
Chirchir-Dan <1danchirchir@gmail.com>
2023-12-04 11:44:45 -0800

    Added 11

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def delete_at(my_list=None, idx=0):
+    if my_list is None or idx < 0 or idx >= len(my_list):
+        return my_list
+
+    new_list = my_list[:idx] + my_list[idx+1:]
+    return new_list
