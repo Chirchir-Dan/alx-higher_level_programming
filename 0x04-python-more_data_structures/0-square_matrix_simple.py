bbc9b0a3de09f9cddca718c0f7bab7e50fc3333b
Chirchir-Dan <1danchirchir@gmail.com>
2023-12-06 10:39:11 -0800

    .

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,11 +1,8 @@
-def square_matrix_alternative(matrix=[]):
-    # Create a new matrix with the same values as the original matrix
-    new_result_matrix = [row[:] for row in matrix]
+#!/usr/bin/python3
 
-    # Iterate through the rows and columns of the new matrix, replacing each element with its square
-    for row_idx, row in enumerate(new_result_matrix):
-        for col_idx, col in enumerate(new_result_matrix):
-            new_result_matrix[row_idx][col_idx] = row[col_idx] ** 2
-
-    # Return the new matrix with squared values
-    return new_result_matrix
+def square_matrix_simple(matrix=[]):
+    new_matrix = [row[:] for row in matrix]
+    for r_index, row in enumerate(new_matrix):
+        for c_index, col in enumerate(new_matrix):
+            new_matrix[r_index][c_index] = row[c_index] ** 2
+    return new_matrix 
