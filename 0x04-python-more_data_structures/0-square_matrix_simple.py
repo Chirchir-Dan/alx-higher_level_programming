3bee47ef3be78388322eb199dc60e3a6db25652e
Chirchir-Dan <1danchirchir@gmail.com>
2023-12-06 10:40:32 -0800

    .

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,7 +2,7 @@
 
 def square_matrix_simple(matrix=[]):
     new_matrix = [row[:] for row in matrix]
-    for r_index, row in enumerate(new_matrix):
-        for c_index, col in enumerate(new_matrix):
-            new_matrix[r_index][c_index] = row[c_index] ** 2
+    for idx, row in enumerate(new_matrix):
+        for idx2, col in enumerate(new_matrix):
+            new_matrix[idx][idx2] = row[idx2] ** 2
     return new_matrix 
