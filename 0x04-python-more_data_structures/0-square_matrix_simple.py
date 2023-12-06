5c0c653b6d88527c8d87a93682ffd14940f7f110
Chirchir-Dan <1danchirchir@gmail.com>
2023-12-06 10:24:54 -0800

    rewrote 0-square..

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,13 +1,13 @@
-def square_matrix_simple(matrix=[]):
-    result_matrix = []
+#!/usr/bin/python3
 
-    for row in matrix:
-        squared_row = []
+def calculate_squared_matrix(input_matrix=[]):
+    # Generate a fresh matrix with matching values to the original matrix
+    result_matrix = [row[:] for row in input_matrix]
 
-        for element in row:
-            squared_element = element ** 2
-            squared_row.append(squared_element)
+    # Iterate through the rows and columns of the result matrix, updating each element with its square
+    for row_index, row in enumerate(result_matrix):
+        for col_index, col in enumerate(result_matrix):
+            result_matrix[row_index][col_index] = row[col_index] ** 2
 
-        result_matrix.append(squared_row)
-
+    # Offer the result matrix with squared values
     return result_matrix
