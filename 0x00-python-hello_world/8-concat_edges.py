729c5ed11d0ece3bb5895a47ccb64bf9045839a8
Chirchir-Dan <1danchirchir@gmail.com>
2023-12-18 05:19:25 -0800

    .

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+print(f"{str.split(' ')[5][:-1]} {str.split(' ')[6]} programming, {str.split(' ')[-6]} with {str.split(' ')[-4]} {str.split(' ')[-2]}")
+
--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
 import random
+
 number = random.randint(-10000, 10000)
 
-# Get the last digit of the number
-last_digit = abs(number) % 10
+# Get the last digit of the number with the correct sign
+last_digit = int(str(number)[-1])
 
-#Determine and print the properties of the last digit
-print(f"Last digit of {number} is {last_digit}", end=" ")
+# Determine and print the properties of the last digit
+output = f"Last digit of {number} is {last_digit}"
 
-if last_digit > 5:
-    print("and is greater than 5")
+if number < 0:
+    output += f" and is less than 6 and not 0" if last_digit < 0 else ""
+elif abs(last_digit) > 5:
+    output += " and is greater than 5"
 elif last_digit == 0:
-    print("and is 0")
-else:
-    print(f"and is less than 6 and not 0")
+    output += " and is 0"
+
+print(output)
+
