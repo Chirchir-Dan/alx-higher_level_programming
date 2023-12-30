d30e67bb642f50ce1864e12624c0d2665a622b43
Chirchir-Dan <1danchirchir@gmail.com>
2023-12-30 00:02:33 -0800

    edited 2-safe to handle traceback

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -9,7 +9,9 @@ def safe_print_list_integers(my_list=[], x=0):
                 print("{:d}".format(my_list[i]), end="")
                 count += 1
         print()  # Add a new line after printing all integers
-    except (IndexError, TypeError):
-        pass  # Handle exceptions by doing nothing
+    except (IndexError, TypeError) as e:
+        import traceback
+        traceback_str = traceback.format_exc()
+        print(traceback_str, end="")
 
     return count
