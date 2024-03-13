de96aa618a73bc4d0a44a5f7167cac6df76deddc
Chirchir-Dan <1danchirchir@gmail.com>
2024-03-13 08:53:22 +0300

    tried a new approach

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 # Prints ASCII alohabet in lowercase
 # Excluding 'q' and 'e' not followed by a new line
-print(''.join(chr(i) for i in range(97, 123) if i not in (101, 113)), end='')
+for i in range(97, 123):
+    if chr(i) != 'q' and chr(i) != 'e':
+        print ("{}".format(chr(i)), end= "")
