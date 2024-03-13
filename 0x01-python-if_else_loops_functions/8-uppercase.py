e3c339fd80d57ac60e0e04981b4c7b81e5e4b2d8
Chirchir-Dan <1danchirchir@gmail.com>
2024-03-13 10:50:05 +0300

    edited 8-upper to comply to max of 2 print functions

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,10 +2,11 @@
 # Prints a string in uppercase followed by a new line.
 
 def uppercase(string):
+    result = ''
     for char in string:
         if ord('a') <= ord(char) <= ord('z'):
             uppercase_char = chr(ord(char) - 32)
-            print("{}".format(uppercase_char), end='')
+            result += "{}".format(uppercase_char)
         else:
-            print("{}".format(char), end='')
-    print()
+            result += "{}".format(char)
+    print(result)
