3be1dccaae94d51fef980eb2ab619bc4e1e1ebaa
Chirchir-Dan <1danchirchir@gmail.com>
2024-03-13 09:17:06 +0300

    trying to correct an error

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 # Print numbers from 0 to 98in decimal and hexadecimal
-for i in range(99):
-    print("{} = Ox{:x}".format(i, i))
+for i in range(0, 99):
+    print("{} = {}".format(i, hex(i)))
