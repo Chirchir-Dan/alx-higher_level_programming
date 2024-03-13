9929fdb1e62b957c5d2ee03fd6d36b63138438cf
Chirchir-Dan <1danchirchir@gmail.com>
2024-03-13 08:56:15 +0300

    corrected pycodestyle errors

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -3,4 +3,4 @@
 # Excluding 'q' and 'e' not followed by a new line
 for i in range(97, 123):
     if chr(i) != 'q' and chr(i) != 'e':
-        print ("{}".format(chr(i)), end= "")
+        print("{}".format(chr(i)), end="")
