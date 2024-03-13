ce73ce390840d8c52e3b5fd26970de0d9292afba
Chirchir-Dan <1danchirchir@gmail.com>
2024-03-13 08:23:42 +0300

    used a single loop

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 # Prints ASCII alohabet in lowercase
 # Excluding 'q' and 'e' not followed by a new line
-for i in range(97, 123):
-    if chr(i) not in ['q', 'e']:
-        print(chr(i), end='')
+print(''.join(chr(i) for i in range(97, 123) if i not in (101, 113)), end='')
