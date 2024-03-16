2c8b5caa646c1b94fa000fba9a5d6973102e6ea8
Chirchir-Dan <1danchirchir@gmail.com>
2024-03-16 07:01:00 +0300

    Added 1-calculation.py

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+def add(a, b):
+    """
+    My addition function
+
+    Args:
+        a: first integer
+        b: second intger
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
+
+
+def sub(a, b):
+    """
+    My substraction function
+
+    Args:
+        a:first integer
+        b: ssecond integer
+    Returns:
+        The return value. a - b
+    """
+    return (a - b)
+
+
+def mul(a, b):
+    """
+    My division function
+
+    Args:
+        a: first argument
+        b: second argument
+
+    Returns:
+        The return value a * b
+    """
+    return (a * b)
+
+
+def div(a, b):
+    """
+    My division function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value a/b
+    """
+    return int(a / b)
