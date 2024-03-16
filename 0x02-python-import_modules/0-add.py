168debea8b6904a8a62e9e6baed717bcfaf0e2d0
Chirchir-Dan <1danchirchir@gmail.com>
2024-03-16 06:25:00 +0300

    Added 0-add.py

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+'''
+Imports the function  def add(a, b): from the file add_0.py
+and prints the result of the addition 1 + 2 = 3
+
+    *uses print function with string format to display integers
+    * The program is not executed when imported - by uding __import__
+'''
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+
+    print("{} + {} = {}".format(a, b, add(a, b)))
