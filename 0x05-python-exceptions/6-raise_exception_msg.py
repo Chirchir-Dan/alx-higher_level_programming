490d2c598bec22a23f63c384b11e719304c5fce5
Chirchir-Dan <1danchirchir@gmail.com>
2024-04-03 11:41:29 +0300

    Added docstring to method in 6-raiseNameError

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -3,4 +3,14 @@
 
 
 def raise_exception_msg(message=""):
-    raise NameError("C is fun")
+    """
+    Raises a name exception with a custom message.
+
+    Args:
+        message(str): The custom message to include in the exception.
+
+    Raises:
+        NameError: Always raised with the specified message.
+    """
+
+    raise NameError(message)
