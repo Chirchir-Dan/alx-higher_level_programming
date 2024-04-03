81d499bd1cc9c0bc1d180ba6bf39d983dd2974c6
Chirchir-Dan <1danchirchir@gmail.com>
2024-04-03 21:48:55 +0300

    Added files 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" The class defines a square based on 0-sqaure.py"""
+
+
+class Square:
+    """"
+    A class to represent a square.
+
+    Attributes:
+        __size (int): The size of the squaare.
+    """
+
+    def __init__(self, size):
+        """ Constructor for Square class.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
