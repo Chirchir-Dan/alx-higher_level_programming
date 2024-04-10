4a76807127131847e314dbce2fb891780c41710c
Chirchir-Dan <1danchirchir@gmail.com>
2024-04-10 09:19:31 +0300

    Added all the files

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ This module provides a function to add two functions. """
 
+
 def add_integer(a, b=98):
     """
     Adds two integers.
@@ -15,9 +16,9 @@ def add_integer(a, b=98):
     Returns:
         int: The sum of a and b as an integer.
     """
-    if not isinstance(a,(int, float)):
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    
+
     return int(a) + int(b)
