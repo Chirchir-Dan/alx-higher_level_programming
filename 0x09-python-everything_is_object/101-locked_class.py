0cc58355d7dbf63565949ccc682707c8350ff8b5
Chirchir-Dan <1danchirchir@gmail.com>
2024-04-11 00:11:02 +0300

    Added docstrings to file 101

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+"""
+This module provides class LockedClass that prevents the user form
+dynamically creating new instance attributes
+"""
+
 
 class LockedClass:
+    """
+    This class prevents dynamic creation of new instance attributes,
+    except for 'first_name'.
+    """
+
     __slots__ = ['first_name']
