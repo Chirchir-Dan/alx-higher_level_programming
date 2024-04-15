67e2a94b17e7c6371b287c2e985ce51d00709d94
Chirchir-Dan <1danchirchir@gmail.com>
2024-04-15 21:41:00 +0300

    Created 0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,15 @@
+"""
+This module contains a function to lookup attributes and methods of an object
+"""
+
+
+def lookup(obj):
+    """
+    Return a list of variable attributes and methods of a n object.
+    Args:
+    obj: The object to look up attributes and method for.
+    Returns:
+    A list containing the names of attributes and methods of the object
+    """
+
+    return dir(obj)
