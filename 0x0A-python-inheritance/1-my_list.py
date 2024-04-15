3ec38376183a7e32bc6a4f5430bae0c77843b8b2
Chirchir-Dan <1danchirchir@gmail.com>
2024-04-15 22:33:49 +0300

    Imported MyList class in test file 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-This module contains the MyList class, a custom list class that inherits from the built-in list class.
+This module contains the MyList class,
+a custom list class that inherits from the built-in list class.
 """
 
 
@@ -8,7 +9,8 @@ class MyList(list):
     """
     A custom list class that inherits from the built-in list class.
     Public methods:
-    print_sorted(self): Print the elements of the list sorted in ascending order.
+    print_sorted(self): Print the elements of the list
+    sorted in ascending order.
     """
     def print_sorted(self):
         """
