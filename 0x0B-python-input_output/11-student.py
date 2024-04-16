7533d2c5b4959464a653baaae1ad40a2667667fe
Chirchir-Dan <1danchirchir@gmail.com>
2024-04-17 00:34:36 +0300

    Created file 11-student.py

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""Module defining the class Student based on 10-student.py"""
+
+
+class Student:
+    """
+    Class that defines properties of student.
+
+    Attributes:
+        first_name (str): first name of student.
+        last_name (str): last name of student.
+        age (int): age of student.
+    """
+    def __init__(self, first_name, last_name, age):
+        """Creates new instances of Student.
+
+        Args:
+            first_name (str): first name of student.
+            last_name (str): last name of student.
+            age (int): age of student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance.
+
+        If attrs is a list of strings, only attribute names contained in,
+        this list must be retrieved.
+        Otherwise, all attributes must be retrieved.
+
+        Args:
+            attrs (list): A list of strings containing attribute names.
+
+        Returns:
+            dict: A dictionary containing the attributes of the \
+                    Student instance.
+        """
+        if attrs is None:
+            return self.__dict__
+
+        student_dict = {}
+        for attr in attrs:
+            if attr in self.__dict__:
+                student_dict[attr] = getattr(self, attr)
+        return student_dict
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student instance with \
+                the values from json.
+
+        Args:
+            json (dict): A dictionary containing attribute names \
+                    and their values.
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
