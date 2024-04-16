a1860d41ac7902b27df0547076e4456361238e20
Chirchir-Dan <1danchirchir@gmail.com>
2024-04-16 20:54:42 +0300

    Created 0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This module provides a function that reads a text file and prints itto stdout
+"""
+
+
+def read_file(filename=""):
+    """
+    Reads a text file (UTF8) and prints it to stdout
+
+    Args:
+        filename (str): name of the file to read from
+    Returns:
+        A list of text.
+    """
+
+    with open(filename, 'r', encoding="utf-8") as f:
+        content = f.read()
+        print(content)
