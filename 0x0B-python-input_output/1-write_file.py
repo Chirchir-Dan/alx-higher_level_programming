dbfd990de7c6ae84274dd9d0b890f551cf9c60a2
Chirchir-Dan <1danchirchir@gmail.com>
2024-04-16 21:46:03 +0300

    Added method docstring and split a long comment into two line

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 """
-This module provides a function that writes a string to a text file and returns the nunber of characters written
+This module provides a function that writes a string to \
+        a text file and returns the nunber of characters written
 """
 
 
 def write_file(filename="", text=""):
     """
+    This function writes a string to a text file and returns the \
+            number of characters written
     """
+
     with open(filename, 'w', encoding="utf-8") as file:
         no_chars = file.write(text)
         return no_chars
