57d6bc8f9436f0e0bb0405bd2086c36bf15a4018
Chirchir-Dan <1danchirchir@gmail.com>
2024-04-16 22:00:12 +0300

    Created 2-append_write.py

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This module provides a function that appends a string at the \
+        end of a text file.
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    Appends a string at the end of a text file
+    
+    Args:
+        filename (str): The name of the file to append to
+        text (str): The string to append to the file.
+
+    Returns:
+        The number of characters appended.
+    """
+
+    with open(filename, 'a', encoding="utf-8") as file:
+        return file.write(text)
