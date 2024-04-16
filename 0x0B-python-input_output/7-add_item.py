cb9a519b7c72fc106748d6417835e9b3117185c5
Chirchir-Dan <1danchirchir@gmail.com>
2024-04-16 23:48:30 +0300

    Added amethod definition to script 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,20 +1,35 @@
 #!/usr/bin/python3
 """
-This module adds command-line arguments to a pyhton list \
-    and saves them to a JSON file
+This script adds command-line arguments to a Python list and \
+        saves them to a JSON file.
+
+If the file 'add_item.json' exists, it loads the existing list from the file.
+If the file doesn't exist, it initializes an empty list.
+Then, it appends the command-line arguments to the list.
+Finally, it saves the updated list to the file in JSON format.
 """
 
 
 import sys
-
 save_file = __import__('5-save_to_json_file').save_to_json_file
 load_obj = __import__('6-load_from_json_file').load_from_json_file
 
-args = sys.argv[1:]
 
-# Load the existing List or initialize an empty list
-items_list = load_obj("add_item.json") if args else []
+def add_items_to_list_and_save():
+    """
+    This method adds command-line arguments to a pyhton list \
+    and saves them to a JSON file
+    """
 
-items_list.extend(args)
+    args = sys.argv[1:]
 
-save_file(items_list, "add_item.json")
+    # Load the existing List or initialize an empty list
+    items_list = load_obj("add_item.json") if args else []
+
+    items_list.extend(args)
+
+    save_file(items_list, "add_item.json")
+
+
+if __name__ == "__main__":
+    add_items_to_list_and_save()
