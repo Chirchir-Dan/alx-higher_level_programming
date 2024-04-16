d13576ce5c47c30961a35ca0e3e8eff18c4fc944
Chirchir-Dan <1danchirchir@gmail.com>
2024-04-17 00:28:21 +0300

    Tried without using try except

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,42 +1,46 @@
 #!/usr/bin/python3
-"""Module containing the Student class"""
+"""Module defining the class Student based on 9-student.py"""
 
 
 class Student:
     """
-    Defines a student by first_name, last_name, and age.
+    Class that defines properties of student.
 
     Attributes:
-        first_name (str): The first name of the student.
-        last_name (str): The last name of the student.
-        age (int): The age of the student.
+        first_name (str): first name of student.
+        last_name (str): last name of student.
+        age (int): age of student.
     """
-
     def __init__(self, first_name, last_name, age):
-        """Initializes a new Student instance."""
+        """Creates new instances of Student.
+
+        Args:
+            first_name (str): first name of student.
+            last_name (str): last name of student.
+            age (int): age of student.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """
-        Retrieves a dictionary representation of a Student instance.
+        """Retrieves a dictionary representation of a Student instance.
+
+        If attrs is a list of strings, only attribute names contained in,
+        this list must be retrieved.
+        Otherwise, all attributes must be retrieved.
 
         Args:
             attrs (list): A list of strings containing attribute names.
 
         Returns:
-            dict: A dictionary containing the attributes of the \
-                    Student instance.
+            dict: A dictionary containing the attributes of the Student instance.
         """
-
         if attrs is None:
             return self.__dict__
-
+        
         student_dict = {}
         for attr in attrs:
-            try:
-                new_dict[item] = self.__dict__[item]
-            except Exception:
-                pass
+            if attr in self.__dict__:
+                student_dict[attr] = getattr(self, attr)
         return student_dict
