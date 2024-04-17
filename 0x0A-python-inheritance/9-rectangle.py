8ca95d1b13d18d1408df625cfb7c17c88e283c78
Chirchir-Dan <1danchirchir@gmail.com>
2024-04-17 19:03:54 +0300

    Created file 9-rectangle.py

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""
+Module: 8-rectangle
+
+This module contains the definition of the Rectangle class,
+which inherits from BaseGeometry.
+"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Class representing a rectangle shape.
+
+    Attributes:
+        __width (int): The width of the rectangle.
+        __height (int): The height of the rectangle.
+    """
+
+    def __init__(self, width, height):
+        """
+        Initialize a rectangle with width and height.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+        """
+        super().__init__()
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+    def area(self):
+        """
+        Calculate the area of the rectangle.
+
+        Returns:
+            int: The area of the rectangle.
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """
+        Return the string representation of the rectangle.
+
+        Returns:
+            str: The string representation of the rectangle.
+        """
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
+    def print(self):
+        """
+        Print the rectangle description.
+        """
+        print(self.__str__())
