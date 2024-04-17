f26b635e01dd2ca5353b7d976dd451e937524917
Chirchir-Dan <1danchirchir@gmail.com>
2024-04-17 19:11:37 +0300

    Overrided __dir__() method in the Rectangle class

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -26,7 +26,7 @@ class Rectangle(BaseGeometry):
             width (int): The width of the rectangle.
             height (int): The height of the rectangle.
         """
-
+        super().__init__()
         self.__width = width
         self.__height = height
         self.integer_validator("width", width)
@@ -55,3 +55,14 @@ class Rectangle(BaseGeometry):
         Print the rectangle description.
         """
         print(self.__str__())
+
+    def __dir__(self):
+        """
+        Override the __dir__() method to customize the list of
+        attributes and methods.
+
+        Returns:
+            list: List of attributes and methods specific to the
+            Rectangle class.
+        """
+        return [attr for attr in dir(self) if not attr.startswith('_')]
