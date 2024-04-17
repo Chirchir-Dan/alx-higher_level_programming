f93ff47e8702ddf38d540aecb1eeeb7666b1acf7
Chirchir-Dan <1danchirchir@gmail.com>
2024-04-17 17:58:57 +0300

    Corrected the class prototype

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This module provides a class that inherits from a parent class
+"""
+
+
+class BaseGeometry:
+    """
+    class BaseGeometry
+    """
+
+    def area(self):
+        """
+        Doesn't have an implementation yet
+        """
+
+        raise Exception("area() is not implemented")
