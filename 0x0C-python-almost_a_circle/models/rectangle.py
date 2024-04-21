56589d48397e2c0f0f57b86fd03f1627f355f2aa
Chirchir-Dan <1danchirchir@gmail.com>
2024-04-21 19:18:30 +0300

    Added public method update that takes a variable no. of positional arguments

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -158,3 +158,26 @@ class Rectangle(Base):
         """
         return ("[Rectangle] ({}) {}/{} - {}/{}".format(
                 self.id, self.x, self.y, self.width, self.height))
+
+    def update(self, *args):
+        """
+        Assigns arguments to each attribute.
+
+        Args:
+            *args: The arguments to assign.
+                1st argument: id attribute
+                2nd argument: width attribute
+                3rd argument: height attribute
+                4th argument: x attribute
+                5th argument: y attribute
+        """
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.width = args[1]
+        if len(args) >= 3:
+            self.height = args[2]
+        if len(args) >= 4:
+            self.x = args[3]
+        if len(args) >= 5:
+            self.y = args[4]
