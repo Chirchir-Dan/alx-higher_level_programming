c414f38f83a816b093af53a555a4d8bc589367fa
Chirchir-Dan <1danchirchir@gmail.com>
2024-05-22 22:57:48 +0300

    Edited some scripts

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,12 +1,13 @@
--- 8-list_california_cities.sql
--- This script lists all the cities of California found in the database hbtn_0d_usa.
--- It retrieves cities from the cities table by filtering with the state_id obtained from the states table where name = California.
+-- cities_of_california.sql
+-- This script lists all the cities of California from the database hbtn_0d_usa.
+-- The states table contains only one record where name = California.
 -- Results are sorted in ascending order by cities.id.
--- The database name will be passed as an argument of the mysql command.
+-- JOIN keyword is not used.
 
 USE $1;
 
-SELECT cities.*
+SELECT cities.id, cities.name
 FROM cities, states
-WHERE states.name = 'California' AND cities.state_id = states.id
+WHERE cities.state_id = states.id
+AND states.name = 'California'
 ORDER BY cities.id ASC;
