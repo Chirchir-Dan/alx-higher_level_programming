77466cb10b2da62ddaff4aaf217e2664ecc66c53
Chirchir-Dan <1danchirchir@gmail.com>
2024-05-22 23:29:59 +0300

    Edited 9 and 10

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -6,5 +6,6 @@
 
 SELECT cities.id, cities.name, states.name
 FROM cities
-JOIN states ON cities.state_id = states.id
-ORDER BY cities.id ASC;
+LEFT JOIN states
+ON states.id = cities.state_id
+ORDER BY cities.id;
